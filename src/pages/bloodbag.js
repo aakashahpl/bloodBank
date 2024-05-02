e251668e30691b4250ea6810476785216dc2c4d5
Abhinav <abhinavprajapati@gmail.com>
2024-05-03 00:09:09 +0530

    Update blood bag card hover effect

--- src/pages/bloodbag.js
+++ src/pages/bloodbag.js
@@ -64,7 +64,7 @@ export default function Home() {
 
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {bloodBankData.map((bank) => (
-                            <div key={bank.id} className=" bg-gray-800 rounded-lg shadow-md p-6">
+                            <div key={bank.id} className=" bg-gray-800 rounded-lg shadow-md p-6 hover:scale-105 hover:transition ease-in-out">
                                 <h2 className="text-xl font-bold mb-2">{bank.bagid}</h2>
                                 <p className="text-gray-400 mb-2">Blood Type: {bank.bloodtype}</p>
                                 <p className="text-gray-400">Expiry Date: {bank.expdate ? new Date(bank.expdate).toLocaleDateString() : '-'}</p>
